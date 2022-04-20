9374cf1a59877430bc0305b2f40494c6ae1e3323
camdenAlpert <camden.alpert@student.allenisd.org>
2022-04-20 14:26:34 -0500

    Finished reading file function, added vectors for data

--- main.cpp
+++ main.cpp
@@ -12,16 +12,33 @@
 #include "animal.h"
 using namespace std;
 
+vector<string> origData;
+vector<vector<string>> separatedData;
+
 void readDataFile(string fileName) {
     ifstream infile;
     infile.open(fileName);
+    string line;
     while (infile) {
+        if (infile.peek() == 'A') {
+            getline(infile, line);
+        }
+        getline(infile,line);
+        origData.push_back(line);
+        cout << line << endl;
+    }
+}
+
+void animalCreator() {
+    //commas followed by non alpha are not initialized
+    for (int i = 0; i < origData.size(); i++) {
 
     }
 }
 int main() {
     string fileName = "Animal Shelter Data-05.csv";
     readDataFile(fileName);
+    animalCreator();
     // cout << "Data file name to read from: " << endl;
     // cin >> fileName;
 }
