ae0b44f245a387338e44fe040db930c62207b9a1
dcsajdak <dcsajdak@gmail.com>
2022-04-20 13:14:57 -0500

    change log

--- HW6-CatClassFile-DavidSajdak.cpp
+++ HW6-CatClassFile-DavidSajdak.cpp
@@ -1,6 +1,17 @@
-//
-// Created by David on 4/20/2022.
-//
+/*
+    Cat Class for Animal Shelter
+    David Sajdak
+    4/20/22 to 4/27/22
+    CS1337.010
+
+    Purpose: 
+    Create cat class file for HW6
+
+    Change Log and Features:
+    4/20/22 --> 
+
+    Notes:
+*/
 
 #include <iostream>
 using namespace std;
