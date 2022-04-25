3ce50bf78fb6a08db0d36998dd1a388666bb2ad5
a0-10 <abe123210@gmail.com>
2022-04-24 20:28:38 -0500

    Created 2 dog files

--- dog.h
+++ dog.h
@@ -1,13 +1,37 @@
-//
-// Created by Camden on 4/20/2022.
-//
+
+/*
+    Dog Class file for Animal Shelter
+    Abraham Cherian
+    4/20/22 to 4/27/22
+    CS1337.010
+
+    Purpose:
+    Create dog class file for HW6
+
+    Change Log and Features:
+    4/24/22 --> created Dog class (subclass of Animal)
+                added data members
+
+    Notes:
+*/
 
 #ifndef ANIMALSHELTER_DOG_H
 #define ANIMALSHELTER_DOG_H
 
+#include "animal.h"
+#include <string>
 
-class dog {
+using namespace std;
 
+
+class Dog : public Animal {
+private:
+    static int nbrOfDogs;
+    int dogNbr;
+public:
+    Dog();
+    Dog(int);
+    void setDogNbr(int);
 };
 
 
